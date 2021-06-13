45949824ee53bfa7df9ddd869c1727a08146bb08
Eric Norman <eric@norman.zone>
2021-06-12 22:30:29 -0700

    WIP. Complete subtle crypto methods

--- packages/collabswarm/src/auth-subtle-crypto.ts
+++ packages/collabswarm/src/auth-subtle-crypto.ts
@@ -5,84 +5,105 @@
 const concatTypedArray = require('concat-typed-array');
 let subtle = window.crypto.subtle;
 let getRandomValues = window.crypto.getRandomValues;
-
+import { resourceLimits } from "worker_threads";
 import { AuthProvider } from "./auth-provider";
+
+const CRYPTO_ALGORITHM = 'AES-GCM';
+
 
 export class SubtleCrypto<PrivateKey, PublicKey> implements AuthProvider<PrivateKey, PublicKey> {
 
-  public importKey(rawKey: Uint8Array) {
-    /*
-    Import an AES secret key from an ArrayBuffer containing the raw bytes.
-    Takes an ArrayBuffer string containing the bytes, and returns a Promise
-    that will resolve to a CryptoKey representing the secret key.
-    */
+  /// Given a raw key, return the CryptoKey type needed for crypto operations
+  private async _importKey(rawKey: PrivateKey | PublicKey): Promise<CryptoKey> {
     return subtle.importKey(
       "raw",
       rawKey,
-      "AES-GCM",
+      CRYPTO_ALGORITHM,
       true,
-      ["encrypt", "decrypt"]
+      ["sign", "verify"]
     );
   }
 
   
   // Given serialized data,
   // return as serialized data with signature included
-  async sign(data: Uint8Array, privateKey: PrivateKey): Promise<Uint8Array> {
+  public async sign(data: Uint8Array, privateKey: PrivateKey): Promise<Uint8Array> {
     // data expects serialized 
     // privateKey expects AES or HMAC secret keys as ArrayBuffer
-    let cryptoKey: CryptoKey = await subtle.importKey(
-      "raw",
-      privateKey,
-      'AES-GCM',
-      true,
-      ["sign"]
-    );
     let signature = await window.crypto.subtle.sign(
       {
         name: "ECDSA",
         hash: {name: "SHA-256"},
       },
-      cryptoKey,
+      await this._importKey(privateKey),
       data
     );
     return new Uint8Array(signature);
   }
 
-  verify(data: Uint8Array, publicKey: PublicKey, signature: string): Promise<boolean> {
-    throw new Error("Method not implemented.");
+  public async verify(data: Uint8Array, publicKey: PublicKey): Promise<boolean> {
+    // data = sig_length, sig, data array
+    // algorithm must match that used in sign()
+    let sig_length = data[0];
+    let signature = data.slice(1,sig_length);
+    try {
+      return await window.crypto.subtle.verify(
+        CRYPTO_ALGORITHM,
+        await this._importKey(publicKey),
+        signature,
+        data
+      );
+    } catch (err) {
+      console.error("encryption key is not a key for the requested verifying algorithm or when trying to use an algorithm that is either unknown or isn't suitable for a verify operation")
+      throw err;
+    }
   }
 
-  async encrypt(data: Uint8Array, signature: Uint8Array, documentKey: string): Promise<Uint8Array> {
+  // Given a key as a string, return the subtle crypto CryptoKey version
+  // Hard code permissions since only used for these two functions
+  // Here, the key is an ArrayBuffer
+  private async _GetCryptoKeyFrom(documentKey: string): Promise<CryptoKey> {
+    let encoder = new TextEncoder()
+    return await subtle.importKey(
+      "raw",  // format
+      encoder.encode(documentKey),  // keyData: ArrayBuffer
+      CRYPTO_ALGORITHM,
+      true, // extractable
+      ["encrypt", "decrypt"]
+    );
+  }
+
+  public async encrypt(data: Uint8Array, signature: Uint8Array, documentKey: string): Promise<Uint8Array> {
     // documentKey: AES or HMAC secret key from an ArrayBuffer containing the raw bytes
     // returns array starting with IV, where the message contains the signature
-    let algorithm = 'AES-GCM';
-    let data_and_sig = concatTypedArray(Uint8Array, signature.length, signature, data); // here
-    let rawKey = new Uint8Array(new Buffer(documentKey));
-    let cryptoKey = await subtle.importKey(
-      "raw",
-      rawKey,
-      algorithm,
-      true,
-      ["encrypt"]
-    );
-    let iv = getRandomValues(new Uint8Array(12));
+    let data_and_sig = concatTypedArray(Uint8Array, Uint8Array.of(signature.length), signature, data);
+    let encoder = new TextEncoder()
+    let initialVector = getRandomValues(new Uint8Array(12));
     let cipherText = await subtle.encrypt(
-      { name: algorithm, iv: iv },
-      cryptoKey,
+      { 
+        name: CRYPTO_ALGORITHM, 
+        iv: initialVector 
+      },
+      await this._GetCryptoKeyFrom(documentKey),
       data_and_sig
     );
     // TODO (eric) confirm sig.length is always 1 byte (i.e. < 256=2^8)
-    return concatTypedArray(Uint8Array, iv, cipherText);
+    return concatTypedArray(Uint8Array, initialVector, cipherText);
   }
 
-  decrypt(data: Uint8Array, documentKey: string): Promise<Uint8Array> {
+  public async decrypt(data: Uint8Array, documentKey: string): Promise<Uint8Array> {
     // data of form: iv, cipherText
-    // cipherText of form: sig_length, sig, data array
-    let iv = data.slice(0,13);
-    data = data.slice(13)
-    // TODO decrypt with IV
-    // verify here?
-    throw new Error("Method not implemented.");
+    // _initialVector must match the one used to encrypt
+    let initialVector = data.slice(0,13);
+    let cipherText = data.slice(13,);
+    let arrayBuffer = await subtle.decrypt(
+      {
+        name: CRYPTO_ALGORITHM,
+        iv: initialVector
+      },
+      await this._GetCryptoKeyFrom(documentKey),
+      cipherText
+    );
+    return new Uint8Array(arrayBuffer);
   }
 }
