4aee1918121cc9e675af98b344a83397e6f6c1a8
Eric Norman <eric@norman.zone>
2021-06-14 14:18:58 -0700

    More tests

--- packages/collabswarm/src/json-serializer.test.ts
+++ packages/collabswarm/src/json-serializer.test.ts
@@ -3,10 +3,12 @@ import { JSONSerializer } from './json-serializer';
 
 const json_serializer = new JSONSerializer<any, any>();
 
-let test_object = { "key": "val" }
-let test_object_serialized = '{"key":"val"}'
+let test_object = { "key": "val" };
+let test_object_serialized = '{"key":"val"}';
+let test_string = "Hello";
+let test_string_as_u8_array = Uint8Array.from([72,101,108,108,111]);
 
-test('serializes json object to type string', () =>{
+test('serialize json object to string', () =>{
   expect(json_serializer.serialize(test_object))
     .toMatch(test_object_serialized);
 })
@@ -16,3 +18,12 @@ test('deserialize string to json object', () =>{
     .toMatchObject(test_object);
 })
 
+test('encode string to Uint8Array', () =>{
+  expect(json_serializer.encode(test_string))
+    .toStrictEqual(test_string_as_u8_array);
+})
+
+test('decode Uint8Array to string', () =>{
+  expect(json_serializer.decode(test_string_as_u8_array))
+    .toMatch(test_string);
+})
