eced0a7d74daedc81cdba85fea0bb81c7505073e
Eric Norman <eric@norman.zone>
2021-06-16 11:32:41 -0700

    Const for constants

--- packages/collabswarm/src/json-serializer.test.ts
+++ packages/collabswarm/src/json-serializer.test.ts
@@ -6,7 +6,7 @@ const json_serializer = new JSONSerializer<any, any>();
 const test_object = { key: "val" };
 const test_object_serialized = '{"key":"val"}';
 const test_string = "Hello";
-let test_string_as_u8_array = Uint8Array.from([72, 101, 108, 108, 111]);
+const test_string_as_u8_array = Uint8Array.from([72, 101, 108, 108, 111]);
 
 test("serialize json object to string", () => {
   expect(json_serializer.serialize(test_object)).toMatch(
