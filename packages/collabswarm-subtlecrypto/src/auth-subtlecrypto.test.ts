1570cfa36e3e0f567eebca3de6c99d732341285b
Eric Norman <eric@norman.zone>
2021-06-17 15:56:43 -0700

    WIP draft tests for subtlecrypto

--- packages/collabswarm-subtlecrypto/src/auth-subtlecrypto.test.ts
+++ packages/collabswarm-subtlecrypto/src/auth-subtlecrypto.test.ts
@@ -6,7 +6,7 @@ const auth = new SubtleCrypto();
 let keyPair: CryptoKeyPair;
 let documentKey: CryptoKey;
 beforeAll(async () => {
-  keyPair = await window.crypto.subtle.generateKey(
+  keyPair = await crypto.subtle.generateKey(
     {
       name: "ECDSA",
       namedCurve: "P-384",
@@ -14,29 +14,33 @@ beforeAll(async () => {
     true,
     ["sign", "verify"]
   );
-  documentKey;
+  documentKey = await crypto.subtle.generateKey(
+    {
+        name: "AES-GCM",
+        length: 256,
+    },
+    true,
+    ["encrypt", "decrypt"]
+  )
 });
 
+// try keys generated with different algos
 describe("sign and verify", () => {
   test.each([
     [new Uint8Array([11, 12, 250]), keyPair.privateKey, keyPair.publicKey],
     [{ 123: 234, 345: 567 }, '{"123":234,"345":567}'],
-  ])(`serialize object to string`, async (data, privKey, pubKey) => {
+  ])(`sign and verify`, async (data, privKey, pubKey) => {
     const sig = await auth.sign(data, privKey);
     expect(await auth.verify(data, pubKey, sig)).toBe(true);
   });
 });
 
-// describe('encrypt and decrypt', () => {
-//   test.each([
-//     [test_string, test_string_as_u8_array]
-//   ])
-//   ('encode string to Uint8Array', () =>{
-//   expect(json_serializer.encode(test_string))
-//     .toStrictEqual(test_string_as_u8_array);
-// })
-
-// test('decode Uint8Array to string', () =>{
-//   expect(json_serializer.decode(test_string_as_u8_array))
-//     .toMatch(test_string);
-// })})
+describe('encrypt and decrypt', () => {
+  test.each([
+    [new Uint8Array([11, 12, 250]), documentKey]
+  ])('encrypt and decrypt', async (data, documentKey) => {
+    const res = await auth.encrypt(data, documentKey);
+		expect(await auth.decrypt(res.ciphertext, documentKey, res.iv))
+		.toMatch(data);
+	})
+})
