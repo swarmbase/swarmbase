9e8874f6f500fb85a399351ce97fcc8bdf356e73
Eric Norman <eric@norman.zone>
2021-06-17 19:34:31 -0700

    Can test sign & verify

--- packages/collabswarm/src/auth-subtlecrypto.test.ts
+++ packages/collabswarm/src/auth-subtlecrypto.test.ts
@@ -1,39 +1,104 @@
-import { beforeAll, describe, expect, test } from "@jest/globals";
+import { it, beforeAll, describe, expect, test } from "@jest/globals";
 import { SubtleCrypto } from "./auth-subtlecrypto";
-import { setup_keys } from "./auth-subtlecrypto.testsetup";
 
 const auth = new SubtleCrypto();
 
-beforeAll(() => {
-  return setup_keys();
-});
+const privateKeyData1 = {"key_ops":["sign"],"ext":true,"kty":"EC","x":"iV0DESMDz3fcubTpUCMK4YLWbU9gDslDgdflc5OGrQVII_wCViDdqGbMTOmQLY0F","y":"CQyfju2lK2mT0TIVDI-olIqFC3m3AayX0deHkw4JPCU-GwzV9k0BT295OSQ495kK","d":"kr28U5k3zRtFMXAQuoUZgmqnpI0w01p9sh0spOXZBnkc6Ez6rdbN2W6ZcAJBXxge","crv":"P-384"};
+const publicKeyData1 = {"key_ops":["verify"],"ext":true,"kty":"EC","x":"iV0DESMDz3fcubTpUCMK4YLWbU9gDslDgdflc5OGrQVII_wCViDdqGbMTOmQLY0F","y":"CQyfju2lK2mT0TIVDI-olIqFC3m3AayX0deHkw4JPCU-GwzV9k0BT295OSQ495kK","crv":"P-384"};
+const privateKeyData2 = {"key_ops":["sign"],"ext":true,"kty":"EC","x":"oodHRfDRDsXcpe2FvwctaK1y4pt8Lhx5tmiXZ-35vzXuDUD5zWhzPxgC8FZvyY0K","y":"KhgG-mU2-mNbhgdK9_8nEMwPa2_bWWl_zlqY6Q4xuXYMOjhSLGydbFIDSAGBaNaJ","d":"ZtP5zRvBLPK82BAwNs49-Y9227v2vtSdwhgUgH965LTdyZ-9R3qTQEPS7F6vwhyM","crv":"P-384"};
+const publicKeyData2 = {"key_ops":["verify"],"ext":true,"kty":"EC","x":"oodHRfDRDsXcpe2FvwctaK1y4pt8Lhx5tmiXZ-35vzXuDUD5zWhzPxgC8FZvyY0K","y":"KhgG-mU2-mNbhgdK9_8nEMwPa2_bWWl_zlqY6Q4xuXYMOjhSLGydbFIDSAGBaNaJ","crv":"P-384"};
+
+async function importKey(
+  keyData: JsonWebKey,
+  usage: KeyUsage[] = ['sign', 'verify'],
+  format = 'jwk',
+  namedCurve = "P-384",
+) {
+  const key = await crypto.subtle.importKey(format, keyData, {
+    name: 'ECDSA',
+    namedCurve
+  }, true, usage);
+  return key;
+}
+
 // try keys generated with different algos
 describe("sign and verify", () => {
   test.only.each([
-    [new Uint8Array([11, 12, 250]), keyPair.privateKey, keyPair.publicKey],
-    [{ 123: 234, 345: 567 }, '{"123":234,"345":567}'],
-  ])(`sign and verify`, async (data, privKey, pubKey) => {
-    const sig = await auth.sign(data, privKey);
-    expect(await auth.verify(data, pubKey, sig)).toBe(true);
+    [
+      new Uint8Array([11, 12, 250]),
+      privateKeyData1,
+      publicKeyData1,
+      true,
+			false,
+			false,
+    ],
+    [
+      new Uint8Array([11, 44, 250]),
+      privateKeyData1,
+      publicKeyData2,
+      false,
+			false,
+			false,
+    ],
+    [
+      new Uint8Array([11, 44, 250]),
+      publicKeyData1,
+      publicKeyData1,
+      false,
+			true,
+			false
+    ],
+		[
+      new Uint8Array([11, 44, 250]),
+      privateKeyData1,
+      privateKeyData1,
+      false,
+			false,
+			true
+    ],
+  ])(`sign and verify`, async (data: Uint8Array, privateKeyData: JsonWebKey, publicKeyData: JsonWebKey, success: boolean, expectedSignCrashed: boolean, expectedVerifyCrashed: boolean) => {
+    const privateKey = await importKey(privateKeyData, ['sign']);
+    const publicKey = await importKey(publicKeyData, ['verify']);
+    let signCrashed = false;
+    let sig: Uint8Array | undefined;
+    try {
+      sig = await auth.sign(data, privateKey);
+    } catch {
+      signCrashed = true;
+    }
+    expect(signCrashed).toBe(expectedSignCrashed);
+    if (sig !== undefined) {
+      let verifyCrashed = false;
+      let result: boolean | undefined;
+      try {
+        result = await auth.verify(data, publicKey, sig);
+      } catch {
+        verifyCrashed = true;
+      }
+      expect(verifyCrashed).toBe(expectedVerifyCrashed);
+      if (result !== undefined) {
+        expect(result).toBe(success);
+      }
+    }
   });
 });
 
-describe("encrypt and decrypt", async () => {
-  const documentKey = await crypto.subtle.generateKey(
-    {
-      name: "AES-GCM",
-      length: 256,
-    },
-    true,
-    ["encrypt", "decrypt"]
-  );
-  test.each([[new Uint8Array([11, 12, 250]), documentKey]])(
-    "encrypt and decrypt",
-    async (data, documentKey) => {
-      const res = await auth.encrypt(data, documentKey);
-      expect(await auth.decrypt(res.ciphertext, documentKey, res.iv)).toMatch(
-        data
-      );
-    }
-  );
-});
+// describe("encrypt and decrypt", async () => {
+//   const documentKey = await crypto.subtle.generateKey(
+//     {
+//       name: "AES-GCM",
+//       length: 256,
+//     },
+//     true,
+//     ["encrypt", "decrypt"]
+//   );
+//   test.each([[new Uint8Array([11, 12, 250]), documentKey]])(
+//     "encrypt and decrypt",
+//     async (data, documentKey) => {
+//       const res = await auth.encrypt(data, documentKey);
+//       expect(await auth.decrypt(res.ciphertext, documentKey, res.iv)).toMatch(
+//         data
+//       );
+//     }
+//   );
+// });
---
+++ packages/collabswarm/src/utils-test.js
@@ -0,0 +1,39 @@
+// https://nodejs.org/api/webcrypto.html#webcrypto_exporting_and_importing_keys
+
+const { webcrypto } = require("crypto");
+
+async function generateAndExportHmacKey() {
+	const key = await webcrypto.subtle.generateKey(
+		{
+			name: "ECDSA",
+			namedCurve: "P-384",
+		},
+		true,
+		["sign", "verify"]
+	);
+	return [
+		await webcrypto.subtle.exportKey("jwk", key.privateKey),
+		await webcrypto.subtle.exportKey("jwk", key.publicKey),
+	];
+}
+
+async function importHmacKey(keyData, format = "jwk", hash = "SHA-512") {
+	const key = await webcrypto.subtle.importKey(
+		format,
+		keyData,
+		{
+			name: "HMAC",
+			hash,
+		},
+		true,
+		["sign", "verify"]
+	);
+
+	return key;
+}
+
+(async function main() {
+	const [privateKey, publicKey] = await generateAndExportHmacKey();
+	console.log(JSON.stringify(privateKey));
+	console.log(JSON.stringify(publicKey));
+})();
