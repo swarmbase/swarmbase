16c991b45d750aec39a1863e76b8a5b665579417
Robert Chu <allegrormc@gmail.com>
2021-07-03 15:27:25 -0700

    Adds Keychain/KeychainProvider. (#26)
    * Adds Keychain/KeychainProvider.
    
    * Update comments.
    
    * Updated comments.
    
    * Format bd0b4b7
    
    Co-authored-by: prettifier[bot] <45367598+prettifier[bot]@users.noreply.github.com>

---
+++ packages/collabswarm/src/keychain.ts
@@ -0,0 +1,36 @@
+/**
+ * A keychain contains a CollabswarmDocument's encryption keys.
+ *
+ * @tparam KeychainChange Type of a block of change(s) describing edits made to the document keychain.
+ * @tparam DocumentKey Type of a document encryption key.
+ */
+export interface Keychain<KeychainChange, DocumentKey> {
+  /**
+   * Add a document encryption key to the keychain.
+   *
+   * @param key Document encryption key to add.
+   * @return A block of change(s) describing the keychain addition.
+   */
+  add(key: DocumentKey): Promise<KeychainChange>;
+
+  /**
+   * Gets a block of change(s) describing the whole state of the keychain.
+   *
+   * @return A block of change(s) describing the keychain.
+   */
+  history(): KeychainChange;
+
+  /**
+   * Merges in a block of change(s) to the keychain.
+   *
+   * @param change A block of change(s) to apply.
+   */
+  merge(change: KeychainChange): void;
+
+  /**
+   * Gets the all document encryption keys in the keychain.
+   *
+   * @return all document encryption keys.
+   */
+  keys(): Promise<DocumentKey[]>;
+}
