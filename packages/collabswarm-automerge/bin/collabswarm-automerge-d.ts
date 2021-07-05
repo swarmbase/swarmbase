29d415d9c3fbf7d9200b166ba27dca93f3251c90
Robert Chu <allegrormc@gmail.com>
2021-07-05 14:17:48 -0700

    Adds automerge ACL/Keychain implementations. (#28)

--- packages/collabswarm-automerge/bin/collabswarm-automerge-d.ts
+++ packages/collabswarm-automerge/bin/collabswarm-automerge-d.ts
@@ -1,11 +1,25 @@
 #!/usr/bin/env node
 
-import { CollabswarmNode } from '@collabswarm/collabswarm';
+import { CollabswarmNode, SubtleCrypto } from '@collabswarm/collabswarm';
 import { AutomergeJSONSerializer, AutomergeProvider } from '../src';
+import {
+  AutomergeACLProvider,
+  AutomergeKeychainProvider,
+} from '../src/collabswarm-automerge';
 
 console.log('Creating a new swarm node...');
 const crdt = new AutomergeProvider();
 const serializer = new AutomergeJSONSerializer();
-const swarmNode = new CollabswarmNode(crdt, serializer, serializer);
+const auth = new SubtleCrypto();
+const acl = new AutomergeACLProvider();
+const keychain = new AutomergeKeychainProvider();
+const swarmNode = new CollabswarmNode(
+  crdt,
+  serializer,
+  serializer,
+  auth,
+  acl,
+  keychain,
+);
 console.log('Starting node...');
 swarmNode.start();
